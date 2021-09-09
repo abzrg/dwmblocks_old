d10c785d1cb71a4f4f6aa583b6d7418da363e696
Ali Bozorgzadeh <aliiiib95@gmail.com>
2021-09-10 03:49:14 +0430

    change newsboat update mechanism to signal based

--- config.def.h
+++ config.def.h
@@ -1,14 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/    /*Command*/     /*Update Interval*/ /*Update Signal*/
-    {"📰 ",         "sb_news",          60,     0   },
-    {"",            "sb_memory",        10,     0   },
-    {"🧬 ",         "sb_keylayout",     0,      2   },
-    {"",            "sb_nettraf",       2,      0   },
-    {"",            "sb_brightness",    0,      4   },
-    {"",            "sb_volume",        0,      3   },
-    {"",            "sb_battery",       60,     0   },
-    {"",            "sb_clock",         60,     0   },
+    /*Icon*/        /*Command*/     /*Update Interval*/ /*Update Signal*/
+    {"📰 ",         "sb_news",              0,                  6       },
+    {"",            "sb_memory",            5,                  0       },
+    {"🧬 ",         "sb_keylayout",         0,                  2       },
+    {"",            "sb_nettraf",           2,                  0       },
+    {"",            "sb_brightness",        0,                  4       },
+    {"",            "sb_volume",            0,                  3       },
+    {"",            "sb_battery",           60,                 0       },
+    {"",            "sb_clock",             5,                  0       },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
